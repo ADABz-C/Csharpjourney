484bd3fdec2c0786708787f1a6906d29f9f2990f
Chidi <adabanyadavid@gmail.com>
2025-10-02 00:11:26 -0400

    selectionsort update

---
+++ selectionSort/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //System.Console.WriteLine("Hello, World!");
+
+        int[] nums = { 1, 5, 3, 67, 21 };
+
+        
+
+
+        int[] second = { 1, 5, 3 };
+        iterate(second);
+        System.Console.WriteLine();
+        iterate(6, 7, 9, 54);
+        System.Console.WriteLine();
+        selectionSort(nums);
+        iterate(nums);
+    }
+
+    private static void iterate(params int[] nums)
+    {
+        for(int i = 0; i< nums.Length; i++)
+        {
+            System.Console.Write(nums[i]+ ", ");
+        }
+    }
+
+    private static void selectionSort(int[] nums)
+    {
+        for (int i = 0;i< nums.Length - 1 ; i++)
+        {
+            int min = i;
+
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[min] > nums[j])
+                {
+                    min = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min] = temp;
+
+        }
+    }
+}
