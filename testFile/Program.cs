0349edc19e175b872265400ee876f2cb010ea273
Chidi <adabanyadavid@gmail.com>
2025-11-10 12:22:02 -0500

    testfile update

--- testFile/Program.cs
+++ testFile/Program.cs
@@ -1,5 +1,58 @@
-﻿class Program
-{
-    //testing file management
+﻿using System.IO;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //File Operations
+        string path = "C:\\Users\\chide\\source\\repos\\Csharpjourney\\testFile\\files\\example.txt";
+
+        //Reading file
+
+        if (File.Exists(path))
+        {
+            string content = File.ReadAllText(path);
+            Console.WriteLine(content);
+
+        }
+        else
+        {
+            Console.WriteLine("File doesn't exist twin");
+        }
+
+        //Writing to file (overwrite)
+        string writeContent = "New written line";
+        File.WriteAllText(path, writeContent); //(path), (new content)
+                                               //Adding/appending to file
+        string addedContent = "\nI've append a 2nd line";
+        File.AppendAllText(path, addedContent);
+
+        //File Directory and operations
+        string directoryPath = "C:\\Users\\chide\\source\\repos\\Csharpjourney\\testFile\\newFilesFolder\\";
+        string filePath = Path.Combine(directoryPath, "example2.txt");
+        
+
+
+        if (!Directory.Exists(filePath))
+        {
+            Directory.CreateDirectory(directoryPath);
+            Console.WriteLine("Directory Created");
+
+        }
+        File.WriteAllText(filePath, "example 2 created bro");
+
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "The conditional statement created this created");
+            Console.WriteLine("File Created inside directorty");
+        }
+
+
+        //Creating files in the current working directory
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory; // Checks for program's location
+        string newFolderPath = Path.Combine(currentDirectory, "workingFolder"); //adds workingFolder to programs location
+
+
+
+    }
 }
--- testFile/obj/Debug/net9.0/testFile.AssemblyInfo.cs
+++ testFile/obj/Debug/net9.0/testFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484bd3fdec2c0786708787f1a6906d29f9f2990f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81f64492084249bf503104eefa9066c82b52643")]
 [assembly: System.Reflection.AssemblyProductAttribute("testFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
